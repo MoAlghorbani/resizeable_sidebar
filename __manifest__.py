f6b2dd5dc7533da9a9e9c78283868bdaea8a1e66
Mohammed Fadl <mohammadefathl@gmail.com>
2025-04-29 20:11:44 +0300

    init project

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Resizeable Sidebar",
+    "version": "17.0.0.2",
+    "summary": "Resizeable Sidebar",
+    "sequence": 10,
+    "author": "AMT",
+    "depends": ["base", "web"],
+    "installable": True,
+    "data": [],
+    "assets": {
+        "web.assets_backend": [
+            "resizeable_sidebar/static/src/**/*",
+        ],
+    },
+    "license": "LGPL-3",
+}
---
+++ static/src/search_panel/search_panel.js
@@ -0,0 +1,8 @@
+/** @odoo-module **/
+
+import { _t } from "@web/core/l10n/translation";
+import { patch } from "@web/core/utils/patch";
+import { SearchPanel } from "@web/search/search_panel/search_panel";
+patch(SearchPanel.prototype, {
+    
+});
