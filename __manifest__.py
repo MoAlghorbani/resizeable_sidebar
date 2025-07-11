2180b60adaa9a1a34a06dfb9e3465622981da1ca
Mohammed Fadl <mohammadefathl@gmail.com>
2025-07-11 23:38:08 +0300

    configure it for odoo 18

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Resizeable Sidebar",
-    "version": "17.0.0.2",
-    "summary": "Resizeable Sidebar",
-    "sequence": 10,
-    "author": "AMT",
-    "depends": ["base", "web"],
-    "installable": True,
-    "data": [],
-    "assets": {
-        "web.assets_backend": [
-            "resizeable_sidebar/static/src/**/*",
+    'name': 'Resizable Sidebar',
+    'version': '18.0.1.0.0',
+    'summary': 'Allows users to resize the sidebar in Odoo.',
+    'sequence': 10,
+    'description': """
+Resizable Sidebar
+=================
+This module allows users to resize the sidebar by dragging its edge. The chosen width is saved in the browser's local storage.
+    """,
+    'author': 'AMT',
+    'depends': ['web'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'resizeable_sidebar/static/src/**/*',
         ],
     },
-    "license": "LGPL-3",
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
 }
