c655aa4f6eea59bf5ad10606a4fc529a7a38e94a
Mohammed Fadl <mohammadefathl@gmail.com>
2025-07-11 23:31:38 +0300

    refactor: configure manifest for 16

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Resizable Sidebar',
-    'version': '18.0.1.0.0',
+    'version': '16.0.1.0.0',
     'summary': 'Allows users to resize the sidebar in Odoo.',
     'sequence': 10,
     'description': """
@@ -10,20 +10,15 @@ Resizable Sidebar
 This module allows users to resize the sidebar by dragging its edge. The chosen width is saved in the browser's local storage.
     """,
     'author': 'AMT',
-    'website': 'https://www.yourcompany.com',
     'depends': ['web'],
-    'category': 'Usability',
     'data': [],
     'assets': {
         'web.assets_backend': [
             'resizeable_sidebar/static/src/**/*',
         ],
     },
-    'images': ['static/description/icon.png'],
     'installable': True,
     'application': False,
     'auto_install': False,
-    'price': 0.00,
-    'currency': 'EUR',
     'license': 'LGPL-3',
 }
