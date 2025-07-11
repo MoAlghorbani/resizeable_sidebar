e6494fc7e721a4f73605c48b09321c1270b41dc9
Mohammed Fadl <mohammadefathl@gmail.com>
2025-07-11 23:24:11 +0300

    feat: prepare module for Odoo 17.0 release

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Resizeable Sidebar",
-    "version": "17.0.0.2",
-    "summary": "Resizeable Sidebar",
-    "sequence": 10,
-    "author": "AMT",
-    "depends": ["base", "web"],
-    "installable": True,
-    "data": [],
-    "assets": {
-        "web.assets_backend": [
-            "resizeable_sidebar/static/src/**/*",
+    'name': 'Resizable Sidebar',
+    'version': '17.0.1.0.0',
+    'summary': 'Allows users to resize the sidebar in Odoo.',
+    'sequence': 10,
+    'description': """
+Resizable Sidebar
+=================
+This module allows users to resize the sidebar by dragging its edge. The chosen width is saved in the browser's local storage.
+    """,
+    'author': 'AMT',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['web'],
+    'category': 'Usability',
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'resizeable_sidebar/static/src/**/*',
         ],
     },
-    "license": "LGPL-3",
+    'images': ['static/description/icon.png'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'price': 0.00,
+    'currency': 'EUR',
+    'license': 'LGPL-3',
 }
